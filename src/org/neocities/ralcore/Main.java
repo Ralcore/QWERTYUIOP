bad037d2f82fad5ffec996bbd1b92e7b863b05c1
Ralcore <mlipytino@hotmail.co.uk>
2021-01-16 22:23:30 +0000

    notes can now be hit
    hitline is on the left side of the screen.
    the first note can now be hit using the A key.
    replaced old keypress system with an updated version using hashmaps.

--- src/org/neocities/ralcore/Main.java
+++ src/org/neocities/ralcore/Main.java
@@ -1,6 +1,7 @@
 package org.neocities.ralcore;
 
 import processing.core.PApplet;
+import java.util.HashMap;
 
 public class Main extends PApplet {
 
@@ -9,6 +10,13 @@ public class Main extends PApplet {
     // Height offset for the rows.
     int rowOffsetH = 100;
     // Note square height. Also defines the gap between rows, to make it 0.
+    int rowHeight = 50;
+    // Millisecond leniency for a note to get missed...
+    int leniencyMiss = 500;
+    // ...and for a 40...
+    int leniency40 = 250;
+    // ...and for an 80.
+    int leniency80 = 150;
 
     public class Note {
         int time;
@@ -33,103 +41,59 @@ public class Main extends PApplet {
 
     public void settings() {
         size(400, 400);
-        //noStroke();
         smooth();
-        myNote = new Note(2000, 0);
-        myNote2 = new Note(2250, 1);
+        myNote = new Note(2000, (int) 'A');
+        myNote2 = new Note(2250, (int) 'A');
     }
 
     Note myNote, myNote2;
 
     public void draw() {
         background(80);
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i]) {
-                rect(i*10, width/2, 10, 50);
+        if (myNote != null) {
+            myNote.noteTick();
+        }
+        if (myNote2 != null) {
+            myNote2.noteTick();
+        }
+    }
+
+    public void checkNote() {
+        if (!(millis() < myNote.time - leniencyMiss)) {
+            // If the hit time is not outside the miss leniency, then we know the note is a hit.
+            if (keys.get(myNote.key) == null || keys.get(myNote.key) != 'T' || millis() < myNote.time - leniency40) {
+                // If the wrong key is hit, or the note is hit outside the 40 leniency, it's a miss.
+                myNote = null;
+                print("Miss!");
+            } else {
+                print("Hit!");
             }
         }
     }
 
+    // This project uses a modified version of jeremydouglass' solution for checking when multiple keys are pressed or held at once.
+    // It can be found here: https://discourse.processing.org/t/solution-for-repeating-keypressed-while-holding-key/3989/3
 
+    // Within this HashMap, the key is the character, and the value is it's state.
+    HashMap<Integer, Character> keys = new HashMap<Integer, Character>();
 
-    // This project uses a modified version of GoToLoop's solution for checking when multiple keys are pressed or held at once.
-    // It can be found here: http://studio.processingtogether.com/sp/pad/export/ro.91tcpPtI9LrXp
-
-    // Within this array, elements 0-25 will be A-Z, whilst 26 will be UP and 27 will be down.
-    boolean keys[] = new boolean[28];
+    // Sets the value in the HashMap to either:
+    // T - True - Pressed down, can hit notes
+    // B - Blocked - Pressed down but has already hit note, used to block repeating keys
+    // F - False - Not pressed
 
     public void keyPressed() {
-        setKeys(keyCode, true);
-        checkNote()
+        if (keys.containsKey(keyCode)) {
+            // Only set to true if the key is not blocked (preventing repeating keys without releasing them)
+            if (keys.get(keyCode) != 'B') keys.put(keyCode, 'T');
+        } else {
+            keys.put(keyCode, 'T');
+        }
+        checkNote();
     }
 
     public void keyReleased() {
-        setKeys(keyCode, false);
-    }
-
-    // This is seriously the best processing-only solution I could find.
-    // Sets array variable for each key depending on whether a key has been hit or released.
-    boolean setKeys(int k, boolean b) {
-        switch(k){
-            case 'A':
-                return keys[0] = b;
-            case 'B':
-                return keys[1] = b;
-            case 'C':
-                return keys[2] = b;
-            case 'D':
-                return keys[3] = b;
-            case 'E':
-                return keys[4] = b;
-            case 'F':
-                return keys[5] = b;
-            case 'G':
-                return keys[6] = b;
-            case 'H':
-                return keys[7] = b;
-            case 'I':
-                return keys[8] = b;
-            case 'J':
-                return keys[9] = b;
-            case 'K':
-                return keys[10] = b;
-            case 'L':
-                return keys[11] = b;
-            case 'M':
-                return keys[12] = b;
-            case 'N':
-                return keys[13] = b;
-            case 'O':
-                return keys[14] = b;
-            case 'P':
-                return keys[15] = b;
-            case 'Q':
-                return keys[16] = b;
-            case 'R':
-                return keys[17] = b;
-            case 'S':
-                return keys[18] = b;
-            case 'T':
-                return keys[19] = b;
-            case 'U':
-                return keys[20] = b;
-            case 'V':
-                return keys[21] = b;
-            case 'W':
-                return keys[22] = b;
-            case 'X':
-                return keys[23] = b;
-            case 'Y':
-                return keys[24] = b;
-            case 'Z':
-                return keys[25] = b;
-            case UP:
-                return keys[26] = b;
-            case DOWN:
-                return keys[27] = b;
-            default:
-                return b;
-        }
+        keys.put(keyCode, 'F');
     }
 
 }
